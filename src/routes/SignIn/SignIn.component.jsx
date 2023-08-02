0cee19c729cf929405a2cfdbd3fd00b50132da6f
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-02 11:23:32 +0300

    delete signin with google redirect

--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,22 +1,10 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
 import {
-	auth,
 	signInWithGooglePopup,
-	signInWithGoogleRedirect,
 	createUserDocFromAuth,
 } from '../../utils/firebase/firebase.utils';
 import './Signin.styles.scss';
 
 const SignIn = () => {
-	useEffect(() => {
-		(async () => {
-			const res = await getRedirectResult(auth);
-			if (res) {
-				const userDocRef = await createUserDocFromAuth(res.user);
-			}
-		})();
-	}, []);
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocFromAuth(user);
@@ -26,9 +14,6 @@ const SignIn = () => {
 		<>
 			<div className='sign'>SignIn</div>
 			<button onClick={logGoogleUser}>Sign in With Google Popup</button>
-			<button onClick={signInWithGoogleRedirect}>
-				Sign in With Google Redirect
-			</button>
 		</>
 	);
 };
