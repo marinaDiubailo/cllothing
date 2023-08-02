c45cb24256404abaf3415ed3f072781971aa8269
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-02 11:19:15 +0300

    add signin with google redirect example

--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,18 +1,34 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 import {
+	auth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserDocFromAuth,
 } from '../../utils/firebase/firebase.utils';
 import './Signin.styles.scss';
 
 const SignIn = () => {
+	useEffect(() => {
+		(async () => {
+			const res = await getRedirectResult(auth);
+			if (res) {
+				const userDocRef = await createUserDocFromAuth(res.user);
+			}
+		})();
+	}, []);
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocFromAuth(user);
 	};
+
 	return (
 		<>
 			<div className='sign'>SignIn</div>
-			<button onClick={logGoogleUser}>Sign in With Google</button>
+			<button onClick={logGoogleUser}>Sign in With Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>
+				Sign in With Google Redirect
+			</button>
 		</>
 	);
 };
