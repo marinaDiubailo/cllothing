da395eac6e81629c6ddd1112af94522f2b288ee7
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-11 16:51:10 +0300

    set logger and redux-devtools-extesion to development mode only

--- src/store/store.js
+++ src/store/store.js
@@ -5,22 +5,22 @@ import {
 } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = store => next => action => {
-	if (!action.type) {
-		return next(action);
-	}
+// const loggerMiddleware = store => next => action => {
+// 	if (!action.type) {
+// 		return next(action);
+// 	}
 
-	console.log('type: ', action.type);
-	console.log('payload: ', action.payload);
-	console.log('currentState: ', store.getState());
+// 	console.log('type: ', action.type);
+// 	console.log('payload: ', action.payload);
+// 	console.log('currentState: ', store.getState());
 
-	next(action);
+// 	next(action);
 
-	console.log('next: ', store.getState());
-};
+// 	console.log('next: ', store.getState());
+// };
 
 const persistConfig = {
 	key: 'root',
@@ -30,12 +30,20 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(
-	applyMiddleware(...middleWares),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+	Boolean
+); // в режиме продакшн .filter(Boolean) вернет []
+
+const composeEnhancer =
+	(process.env.NODE_ENV === 'development' &&
+		window &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, {}, composedEnhancers);
 
 export const persistor = persistStore(store);
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
