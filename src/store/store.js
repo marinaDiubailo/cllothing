e4e2f70a36848f6e57290f54695a323c63c51d94
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-11 16:15:17 +0300

    set persist gate and its functionality

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,8 @@ import {
 	legacy_createStore as createStore,
 	applyMiddleware,
 } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
@@ -20,10 +22,20 @@ const loggerMiddleware = store => next => action => {
 	console.log('next: ', store.getState());
 };
 
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['userReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 const composedEnhancers = compose(
 	applyMiddleware(...middleWares),
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export const store = createStore(rootReducer, {}, composedEnhancers);
+export const store = createStore(persistedReducer, {}, composedEnhancers);
+
+export const persistor = persistStore(store);
